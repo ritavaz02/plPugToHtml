e13d03a2a998c63f58493d5b23ea7083547a1fd9
Millena Freitas <59456180+freitasmillena@users.noreply.github.com>
2023-05-08 01:34:19 +0100

    + recente
    doctype, classes, nesting, div, self closing tags

---
+++ lexer.py
@@ -0,0 +1,243 @@
+import ply.lex as lex
+from ply.lex import LexToken
+
+# List of token names.
+tokens = (
+    'DOCTYPE',
+    'TAG',
+    'ID',
+    'CLASS',
+    'ATTR',
+    'INDENT',
+    'DEDENT',
+    'TEXT',
+    'COMMENT',
+    'INTERPOLATION',
+    'UNBUFFCOMMENT',
+    'ASSIGN',
+    'TAGINTERPOLATION_START',
+    'TAGINTERPOLATION_END',
+    'TAGINTERPOLATION_TEXT',
+    'TAGINTERPOLATION_TEXT_OPEN',
+    'CONTENT'
+)
+
+states = (
+    ('taginterpolation', 'inclusive'),
+    ('commentblock', 'exclusive')
+)
+
+previous_indents = 0
+indent_stack = [0]
+
+def t_DOCTYPE(t):
+    r'doctype\s+html'
+    return t
+
+def t_commentblock_CONTENT(t):
+    r'[^\s].+'
+    return t
+
+
+# tag name
+def t_ANY_TAG(t):
+    r'[a-zA-Z0-9]+(?![a-zA-Z0-9,])'
+    return t
+
+
+dedent_tokens = []
+
+
+def t_ANY_INDENT_DEDENT(t):
+    r'(?<=\n)\s+'
+    indent = len(t.value.strip('\n')) // 4
+    global dedent_tokens
+    if indent > t.lexer.indent_level:
+        if t.lexer.current_state() == 'INITIAL':
+            t.type = 'INDENT'
+            t.value = indent
+            t.lexer.indent_level = indent
+        else:
+            return
+    elif indent < t.lexer.indent_level:
+        if t.lexer.current_state() == 'commentblock':
+            t.lexer.begin('INITIAL')
+        if t.lexer.current_state() == 'INITIAL':
+            while indent < t.lexer.indent_level:
+                t.type = 'DEDENT'
+                t.value = t.lexer.indent_level - 1
+                t.lexer.indent_level = t.lexer.indent_level - 1
+                dedent_tokens.append(t)
+            # print(dedent_tokens)
+            return dedent_tokens.pop(0) if dedent_tokens else None
+    else:
+        if t.lexer.current_state() == 'commentblock':
+            t.lexer.begin('INITIAL')
+        return
+    return t
+
+# interpolation = #[tag interpolation]
+def t_TAGINTERPOLATION_START(t):
+    r'\s*\#\['
+    t.lexer.stack.append('[')
+    t.lexer.begin('taginterpolation')
+    return t
+
+
+def t_TAGINTERPOLATION_END(t):
+    r'\]'
+    if len(t.lexer.stack) > 0 and t.lexer.stack[-1] == '[':
+        t.lexer.stack.pop()
+        if not '[' in t.lexer.stack:
+            t.lexer.begin('INITIAL')
+        else:
+            t.type = 'TAGINTERPOLATION_TEXT'
+    else:
+        print('Nenhuma tag aberta.')
+    return t
+
+
+def t_taginterpolation_TAGINTERPOLATION_TEXT_OPEN(t):
+    r'\s*\[\s?'
+    t.lexer.stack.append('[')
+    t.type = "TAGINTERPOLATION_TEXT"
+    return t
+
+
+# interpolation = #{interpolation}
+def t_ANY_INTERPOLATION(t):
+    r'\#\{[a-zA-Z0-9-_]+\}'
+    return t
+
+
+def t_ANY_newline(t):
+    r'\n+'
+    t.lexer.lineno += len(t.value)
+
+
+# Regular expression rules for simple tokens
+def t_TEXT(t):
+    r'[\|\s]\s*(?:(?!\#\{)(?!\#\[)[^\n])*'
+    return t
+
+
+# id = #id
+def t_ANY_ID(t):
+    r'\#[a-zA-Z][a-zA-Z0-9-]*'
+    return t
+
+
+# class = .class
+def t_ANY_CLASS(t):
+    r'\.[a-zA-Z][a-zA-Z0-9-_]*'
+    return t
+
+
+# attribute = (attr) - '(' then catch all untill ')' then ')'
+def t_ANY_ATTR(t):
+    r'\([^\)]+\)'
+    return t
+
+
+# unbuffered comment = //-comment
+def t_ANY_UNBUFFCOMMENT(t):
+    r'//-(?P<content>.*)'
+    if len(t.value) > 2:
+        t.lexer.begin('commentblock')
+    t.value = t.lexer.lexmatch.group('content')
+    return t
+
+
+# comment = //comment
+def t_ANY_COMMENT(t):
+    r'//(?P<content>.*)'
+    if len(t.value) > 2:
+        t.lexer.begin('commentblock')
+    t.value = t.lexer.lexmatch.group('content')
+    return t
+
+
+# assignment = =variable
+def t_ANY_ASSIGN(t):
+    r'\=\s*[a-zA-Z0-9-_]+'
+    return t
+
+
+def t_taginterpolation_TAGINTERPOLATION_TEXT(t):
+    r'[^\[\n\]](?:(?!\#\{)(?!\#\[)[^\[\n\]])*'
+    return t
+
+
+def t_eof(t):
+    if lexer.indent_level > 0:
+        t.type = 'DEDENT'
+        t.value = lexer.indent_level - 1
+        lexer.indent_level -= 1
+        return t
+    else:
+        return None
+
+
+# A string containing ignored characters (spaces and tabs)
+t_ignore = ''
+
+
+# # Error handling rule
+def t_ANY_error(t):
+    print("Illegal character '%s'" % t.value[0])
+    t.lexer.skip(1)
+
+
+# Build the lexer
+lexer = lex.lex()
+original_token = lexer.token
+
+
+def custom_token():
+    if dedent_tokens:
+        return dedent_tokens.pop(0)
+    return original_token()
+
+
+lexer.token = custom_token
+lexer.stack = list()
+lexer.indent_level = 0
+last_token_pos = lexer.lexpos
+last_lineno = lexer.lineno
+lexer.flag = 0
+
+# a chave é o numero da linha
+# o valor é o nível de indentação
+
+data = """doctype html
+html
+    head
+        meta(charset="UTF-8")
+        meta(name="viewport", content="width=device-width, initial-scale=1.0")
+        link(rel="stylesheet", href="styles.css")
+        title My Pug Example
+    body
+        //- 
+            cont
+            cont
+        header#id.classe.classe2
+            h1 Welcome to My Pug Example 
+        main
+            section
+                h2 #[b About] me
+                p This is a simple example of a Pug template with nested elements and 4 spaces for indentation.
+            .div.outra
+                .divdentro
+            section
+                h2 Contact
+                ul
+                    li.classeli Email: example@example.com
+                    li Phone: +1 123 456 7890
+        footer
+            p Copyright Â© 2023 - All Rights Reserved
+"""
+
+lexer.input(data)
+total_lines = data.count('\n')
+for tok in lexer:
+    print(tok)
--- yacc.py
+++ yacc.py
@@ -1,262 +1,340 @@
 import ply.yacc as yacc
 
-from lex import tokens
+from lexer import tokens
 
 """
-BOTTOM UP PARSING
-z ::= linelist '$'
+Bottom up parsing
 
-lineList ::= lineList line 
-          | line
-          
-line ::= TAG
-        | TAG block
-        | TAG attributes
-        | TAG attributes block
-        | attributes block
+minipug ::= elemList
+
+elemList ::= elemList elem
+           | epsilon
+
+elem ::= TAG attributes insides INDENT elemList DEDENT
+       | TAG attributes insides
+       | attributes insides INDENT elemList DEDENT
+       | attributes insides
+       | comment
+       | unbuffcomment
+       | DOCTYPE
+
+init ::= TAG
+       | CLASS
+
+comment ::= COMMENT content
+
+unbuffcomment ::= UNBUFFCOMMENT content
+
+content ::= content CONTENT
+          | epsilon
 
 attributes ::= attributes attribute
-             | attribute
+             | epsilon
 
-attribute ::= ID
-             | CLASS
-             | ATTR
-             | ASSIGN
+attribute ::= ATTR
+            | CLASS
+            | ID
 
-block ::= TEXT
-        | COMMENT
-        | UNBUFFCOMMENT
-        | INTERPOLATION
-        | TAGINTERPOLATION_START tag_interpolation TAGINTERPOLATION_END
-        | INDENT lineList DEDENT
+insides ::= insides inside
+          | epsilon
 
-tag_interpolation ::= TAGINTERPOLATION_TEXT
-                     | TAGINTERPOLATION_TEXT_OPEN tag_interpolation TAGINTERPOLATION_END
+inside ::= ASSIGN
+         | INTERPOLATION
+         | tag_interpolation
+         | TEXT
+
+tag_interpolation ::= TAGINTERPOLATION_START TAG attributes insides TAGINTERPOLATION_END
+                    | TAGINTERPOLATION_TEXT
 
 """
 
-data = """html
-    head
-        title= Title
-        p Welcome
+data = """doctype html
+html
+    head.HEAD
+        meta(charset="UTF-8")
+        meta(name="viewport", content="width=device-width, initial-scale=1.0")
+        link(rel="stylesheet", href="styles.css")
+        title My Pug Example
     body
-        h1 Title
-        p Welcome home
-$
-
+        //-
+            cont
+            cont
+        header#id.classe.classe2
+            h1 Welcome to My Pug Example
+        main.main.outra.aaa
+            section.sec
+                .section
+                    p Div
+                h2 #[b About] me
+                p This is a simple example of a Pug template with nested elements and 4 spaces for indentation.
+            .div
+                .divdentro.classedentro
+                    .divdentrooo
+            section
+                h2 Contact
+                ul
+                    li.classeli Email: example@example.com
+                    li Phone: +1 123 456 7890
+        footer
+            p Copyright Â© 2023 - All Rights Reserved
 """
 
-# data = """
-# html
-#     head
-#         title= pageTitle
-#         meta(name='description', content='Example Pug code with 5 levels of hierarchy')
-#         // This is a comment inside the head tag
-#     body
-#         h1#main-heading.main-title Welcome to #[strong my website {inter}]
-#         p #[q(lang="en") This] is some text on the page list [1,2,3] dict {a,b,c}.
-#         //- This is an unbuffered comment
-#         p More text on the page.
-#         ul
-#             li item: #{item}
-#             // This is a comment inside the for loop
-#             //- This is an unbuffered comment inside the ul tag
-#             li
-#                 a(href='#') Click here for more information
-#             li
-#                 a(href='/about') About Us
-#                 //- This is an unbuffered comment inside a li tag !
-# $
-# """
+
+def p_minipug(p):
+    """minipug : elemList"""
+    p[0] = p[1]
+
+    # print("p_minipug", p[0])
 
 
-tag_stack = []
+def p_elemList(p):
+    """elemList : elemList elem"""
+    p[0] = f'{p[1]} {p[2]} '
+    #print("p_elemList", p[0])
 
-def p_linelist(p):
-    '''linelist : linelist line'''
-    tag = p[2]["tag"]
-    if tag_stack and tag_stack[-1] == tag:
-        p[1][-1]["body"].append(p[2])
+
+def p_elemList_elem(p):
+    """elemList : elem"""
+    # print(p[1])
+    p[0] = p[1]
+    #print("p_elemList_elem", p[0])
+
+
+# def p_elemList_epsilon(p):
+#     """elemList : """
+#     print("p_elemList_epsilon", p[0])
+#     p[0] = ''
+
+# def p_elem_div(p):
+#     """elem : attributes insides"""
+#     atts = ''
+#     mydict = dict(p[1])
+#     if p[1]:
+#         for key, value in mydict.items():
+#             if key != 'attr':
+#                 atts += f'{key}="{value}" '
+#             else:
+#                 atts += f'{value}'
+#         p[0] = f'<div {atts}> {p[2]} </div>'
+#     #print("p_elem_div", p[0])
+#
+# def p_elem_div_block(p):
+#     """elem : attributes insides INDENT elemList DEDENT"""
+#     atts = ''
+#     mydict = dict(p[1])
+#     if p[1]:
+#         for key, value in mydict.items():
+#             if key != 'attr':
+#                 atts += f'{key}="{value}" '
+#             else:
+#                 atts += f'{value}'
+#     p[0] = f'<div {atts}>{p[2]} {p[4]}</div>'
+#     #print("p_elem_div_block", p[0])
+
+def p_init_tag(p):
+    """init : TAG"""
+    p[0] = ("tag", p[1])
+
+def p_init_div(p):
+    """init : CLASS"""
+    p[0] = ("div", p[1][1:])
+
+def p_elem_tag(p):
+    """elem : init attributes insides"""
+    atts = ''
+    tag = ''
+    mydict = dict(p[2])
+    if p[1][0] == "tag":
+        tag += p[1][1]
     else:
-        p[1].append({
-            "tag": tag,
-            "body": [p[2]]
-        })
-    tag_stack.append(tag)
+        tag += p[1][0]
+        p[2] = 1
+    if p[2]:
+        if p[1][0] == "div":
+            if "class" in mydict:
+                classes = mydict["class"]
+                mydict["class"] = p[1][1] + ' ' + classes
+            else:
+                mydict["class"] = p[1][1]
+        for key, value in mydict.items():
+            if key != 'attr':
+                atts += f'{key}="{value}" '
+            else:
+                atts += f'{value}'
+    if p[3] == '' and tag != "div":
+        p[0] = f'<{tag} {atts}/>'
+    else:
+        p[0] = f'<{tag} {atts}> {p[3]} </{tag}>'
+    print("p_elem_tag", p[0])
+
+
+def p_elem(p):
+    """elem : init attributes insides INDENT elemList DEDENT"""
+    atts = ''
+    mydict = dict(p[2])
+    tag = ''
+    if p[1][0] == "tag":
+        tag += p[1][1]
+    else:
+        tag += p[1][0]
+        p[2] = 1
+    if p[2]:
+        if p[1][0] == "div":
+            if "class" in mydict:
+                classes = mydict["class"]
+                mydict["class"] = p[1][1] + ' ' + classes
+            else:
+                mydict["class"] = p[1][1]
+        for key, value in mydict.items():
+            if key != 'attr':
+                atts += f'{key}="{value}" '
+            else:
+                atts += f'{value}'
+    p[0] = f'<{tag} {atts}>{p[3]} {p[5]}</{tag}>'
+    #print("p_elem", p[0])
+
+
+
+
+def p_elem_comment(p):
+    """elem : comment"""
     p[0] = p[1]
-    print("p_linelist: ", p[0])
-    # print(p.parser.symstack)
+    # print("p_elem_comment", p[0])
 
-def p_linelist_line(p):
-    '''linelist : line'''
-    # p[0] = [p[1]]
-    p[0] = [{        "tag": p[1]["tag"],
-        "body": []
-    }]
-    if "attributes" in p[1]:
-        p[0][0]["attributes"] = p[1]["attributes"]
-    print("p_linelist_line: ", p[0])
 
-def p_line_tag(p):
-    '''line : TAG'''
-    p[0] = {
-        "tag": p[1]
-    }
-    tag_stack.append(p[1])
-    print("p_element_tag: ", p[0])
+def p_elem_unbuffcomment(p):
+    """elem : UNBUFFCOMMENT content"""
+    #print("p_elem_unbuffcomment")
+    p[0] = ''
 
-def p_line_tag_block(p):
-    '''line : TAG block'''
-    p[0] = {
-        "tag": p[1],
-        "body": [{
-            "tag": p[1],
-            "block": p[2]
-        }]
-    }
-    tag_stack.append(p[1])
-    print("p_element_tag_block: ", p[0])
 
-def p_line_tag_attributes(p):
-    '''line : TAG attributes'''
-    p[0] = {
-        "tag": p[1],
-        "attributes": p[2]
-    }
-    tag_stack.append(p[1])
-    print("p_element_tag_attributes: ", p[0])
+def p_elem_doctype(p):
+    """elem : DOCTYPE"""
+    # print("p_elem_doctype", p[0])
+    p[0] = '<!DOCTYPE html>'
 
-def p_line_tag_attributes_block(p):
-    '''line : TAG attributes block'''
-    p[0] = {
-        "tag": p[1],
-        "attributes": p[2],
-        "block": p[3]
-    }
-    tag_stack.append(p[1])
-    print("p_element_tag_attributes_block: ", p[0])
 
-def p_line_attributes_block(p):
-    '''line : attributes block'''
-    p[0] = {
-        "tag": tag_stack.pop(),
-        "attributes": p[1],
-        "block": p[2]
-    }
-    print("p_element_attributes_block: ", p[0])
+def p_comment(p):
+    """comment : COMMENT content"""
+    p[0] = f'<!--{p[2]}-->'
+    # print("p_comment", p[0])
 
-def p_attributes_attribute(p):
-    '''attributes : attributes attribute'''
-    p[1].append(p[2])
-    p[0] = p[1]
-    print("p_attributes_attribute: ", p[0])
 
-def p_attributes_attribute_single(p):
-    '''attributes : attribute'''
-    p[0] = [p[1]]
-    print("p_attributes_attribute_single: ", p[0])
+def p_content(p):
+    """content : content CONTENT"""
+    p[0] = p[1] + f' {p[2]}'
+    # print("p_content", p[0])
+
+
+def p_content_epsilon(p):
+    """content : """
+    # print("p_content_epsilon", p[0])
+    p[0] = ''
+
+
+def p_attributes(p):
+    """attributes : attributes attribute"""
+    dictatts = {}
+    if p[1]:
+        dictatts = p[1]
+        if p[2][0] in dictatts:
+            atts = dictatts[p[2][0]]
+            dictatts[p[2][0]] = atts + ' ' + p[2][1]
+        else:
+            dictatts[p[2][0]] = p[2][1]
+    else:
+        dictatts[p[2][0]] = p[2][1]
+    p[0] = dictatts
+    #print("p_attributes", p[0])
+
+
+def p_attributes_epsilon(p):
+    """attributes : """
+    p[0] = ''
+
 
 def p_attribute_id(p):
-    '''attribute : ID'''
-    p[0] = {
-      "name": "id",
-      "value": p[1][1:] #remove #
-    }
-    print("p_atribute_id: ", p[0])
+    """attribute : ID"""
+    # p[0] = f'id="{p[1][1:]}" '  # remove #
+    p[0] = ("id", p[1][1:])
+    #print("p_attribute_id", p[0])
+
 
 def p_attribute_class(p):
-  '''attribute : CLASS'''
-  p[0] = {
-    "name": "class",
-    "value": p[1][1:] #remove .
-  }
-  print("p_attribute_class: ", p[0])
+    """attribute : CLASS"""
+    # p[0] = f'class="{p[1][1:]}" '  # remove .
+    p[0] = ("class", p[1][1:])
+    #print("p_attribute_class", p[0])
+
 
 def p_attribute_attr(p):
-  '''attribute : ATTR'''
-  p[0] = {
-    "name": "attr",
-    "value": p[1][1:-1] #remove parêntesis ao redor
-  }
-  print("p_attribute_attr: ", p[0])
+    """attribute : ATTR"""
+    # p[0] = f'{p[1][1:-1]} '  # remove []
+    p[0] = ("attr", f'{p[1][1:-1]}')
+    # print("p_attribute_attr", p[0])
 
-def p_attribute_assign(p):
-  '''attribute : ASSIGN'''
-  p[0] = {
-    "name": "assign",
-    "value": p[1][1:] #remove =
-  }
-  print("p_attribute_assign: ", p[0])
-  
-def p_block_text(p):
-    '''block : TEXT'''
-    p[0] = {
-        "type": "text",
-        "value": p[1]
-    }
-    print("p_block_text: ", p[0])
 
-def p_block_comment(p):
-    '''block : COMMENT'''
-    p[0] = {
-        "type": "comment",
-        "value": p[1]
-    }
-    print("p_block_comment: ", p[0])
+def p_insides(p):
+    """insides : insides inside"""
+    if (p[1]):
+        p[0] = p[1] + ' ' + p[2]
+    else:
+        p[0] = p[2]
+    # print("p_insides", p[0])
 
-def p_block_unbuffcomment(p):
-    '''block : UNBUFFCOMMENT'''
-    p[0] = {
-        "type": "unbuffcomment",
-        "value": p[1]
-    }
-    print("p_block_unbuffcomment: ", p[0])
 
-def p_block_interpolation(p):
-    '''block : INTERPOLATION'''
-    p[0] = {
-        "type": "interpolation",
-        "value": p[1][2:-1] #texto dentro de #{}
-    }
-    print("p_block_interpolation: ", p[0])
+def p_insides_epsilon(p):
+    """insides : """
+    # print("p_insides_epsilon", p[0])
+    p[0] = ''
 
-def p_block_taginterpolation(p):
-    '''block : TAGINTERPOLATION_START tag_interpolation TAGINTERPOLATION_END'''
-    p[0] = {
-        "type": "taginterpolation",
-        "value": p[2],
-    }
-    print("p_block_taginterpolation: ", p[0])
 
-def p_block_indent(p):
-    '''block : INDENT linelist DEDENT'''
-    p[0] = p[2]
-    print("p_block_indent: ", p[0])
+def p_inside_assign(p):
+    """inside : ASSIGN"""
+    p[0] = f'{p[1][1:]}'  # remove =
+    # print("p_inside_assign", p[0])
 
-def p_taginterpolation_text(p):
-    '''tag_interpolation : TAGINTERPOLATION_TEXT'''
-    p[0] = {
-        "type": "text",
-        "value": p[1]
-    }
-    print("p_taginterpolation_text: ", p[0])
 
-def p_taginterpolation_text_open(p):
-    '''tag_interpolation : TAGINTERPOLATION_TEXT_OPEN tag_interpolation TAGINTERPOLATION_END'''
-    p[0] = p[2]
-    print("p_taginterpolation_text_open: ", p[0])
+def p_inside_interpolation(p):
+    """inside : INTERPOLATION"""
+    p[0] = f'{p[1][2:-1]}'  # remove #{}
+    # print("p_inside_interpolation", p[0])
+
+
+def p_inside_tag_interpolation(p):
+    """inside : tag_interpolation"""
+    p[0] = p[1]
+    # print("p_inside_tag_interpolation", p[0])
+
+
+def p_inside_text(p):
+    """inside : TEXT"""
+    p[0] = p[1]
+    # print("p_inside_text", p[0])
+
+
+def p_tag_interpolation(p):
+    """tag_interpolation : TAGINTERPOLATION_START TAG attributes insides TAGINTERPOLATION_END"""
+    p[0] = f'<{p[2]} {p[3]}> {p[4]} </{p[2]}>'
+    # print("p_tag_interpolation", p[0])
+
+
+def p_tag_interpolation_text(p):
+    """tag_interpolation : TAGINTERPOLATION_TEXT"""
+    p[0] = p[1]
+    # print("p_tag_interpolation_text", p[0])
 
 
 def p_error(p):
-  if p:
-    print(f"Syntax error at token {p.type} ({p.value}), line {p.lineno}, column {p.lexpos}")
-  else:
-    print("Syntax error at EOF")
+    if p:
+        print(f"Syntax error at token {p.type} ({p.value}), line {p.lineno}, column {p.lexpos}")
+    else:
+        print("Syntax error at EOF")
+
 
 # parser = yacc.yacc()
 parser = yacc.yacc(debug=True)
 output = parser.parse(data)
-
 print("\noutput: ")
 print(output)
 
